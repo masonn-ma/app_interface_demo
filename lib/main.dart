684c7bb57fcead9336a51ae7b9109939a91e1e5b
Mason Ma <107304088+masonn-ma@users.noreply.github.com>
2026-03-31 16:31:46 +0700

    added header

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'res/header.dart';
 
 void main() {
   runApp(const MainApp());
+}
+
+class AppColors {
+  static const bgColor = Color(0xFF101C22);
 }
 
 class MainApp extends StatelessWidget {
@@ -11,10 +16,27 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        backgroundColor: AppColors.bgColor,
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 24.0),
+          child: SafeArea(
+            child: Skeleton(),
+          ),
         ),
       ),
+    );
+  }
+}
+
+class Skeleton extends StatelessWidget {
+  const Skeleton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Header(),
+      ]
     );
   }
 }
---
+++ lib/res/header.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+const iconBgColor = Color(0xFF1E293B);
+
+class Header extends StatelessWidget {
+  const Header({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        const UserIcon(),
+        const WelcomeMessage(text: "Chào buổi sáng, ???"),
+        const NotiIcon(),
+      ]
+    );
+  }
+}
+
+class UserIcon extends StatelessWidget {
+  const UserIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 40,
+      height: 40,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        shape: BoxShape.circle,
+      ),
+      child: const Icon(Icons.person, color: Colors.black),
+    );
+  }
+}
+
+class NotiIcon extends StatelessWidget {
+  const NotiIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: iconBgColor,
+        shape: BoxShape.circle,
+      ),
+      child: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.notifications_outlined, color: Colors.white),
+          ),
+    );
+  }
+}
+
+class WelcomeMessage extends StatelessWidget {
+  final String text;
+
+  const WelcomeMessage({
+    super.key, 
+    required this.text
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(Icons.wb_sunny_outlined, color: Colors.white),
+        SizedBox(width: 8.0),
+        Text(
+          text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18.0,
+          ),
+        ),
+      ],
+    );
+  }
+}
